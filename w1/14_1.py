32cabbae810b4b62f42a42d06f4be480f34240ab
bobur554396 <bobur.muhsimbaev@gmail.com>
2021-01-30 16:15:49 +0600

    changed

--- w1/14_1.py
+++ w1/14_1.py
@@ -1,4 +1,23 @@
 """
 3 5 4 2
 """
+n = int(input())
+
+nums = input().split()
+
+if n != len(nums):
+  print('error')
+else:
+  print('it is ok you can continue')
+
+# print(input().split())
+
+# s = 0
+# for i in input().split():
+#   s += int(i)
+
+# print(sum([1, 2.4, 3]))
+
+# print([int(i) for i in input().split()])
+
 print(sum([int(i) for i in input().split()]))
--- w1/18.py
+++ w1/18.py
@@ -1,8 +1,12 @@
 a = 'Hello Kbtu'
 
 for i, v in enumerate(a):
+  # print(i[0], i[1])
+  # print(type(i))
   print(f"{i} ---> {v}")
 
+# [0, 'a']
+# (0, 'a')
 
 print(a.upper())
 print(a.lower())
